a7d6791c97a0610d64a651af619f48c3e6254810
Zachary <duvallzt@gmail.com>
2021-03-03 16:25:52 -0700

    Make dropdown disappear when click links

--- react-app/src/components/NavBar/Dropdown.js
+++ react-app/src/components/NavBar/Dropdown.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
-
-export default function Dropdown({ dropdownLinks }) {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const openMenu = () => {
-    setShowMenu((prevShow) => !prevShow);
+export default function Dropdown({
+  dropdownLinks,
+  showDropdown,
+  setShowDropdown,
+}) {
+  const toggleDropdown = () => {
+    setShowDropdown((prevShow) => !prevShow);
   };
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button onClick={toggleDropdown}>
         <i
           className={
-            showMenu ? 'far fa-window-close fa-lg' : 'fas fa-bars fa-lg'
+            showDropdown ? 'far fa-window-close fa-lg' : 'fas fa-bars fa-lg'
           }
         ></i>
       </button>
-      {showMenu && <ul className='navbar__dropdown'>{dropdownLinks}</ul>}
+      {showDropdown && <ul className='nav__dropdown'>{dropdownLinks}</ul>}
     </>
   );
 }
--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,52 +1,74 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 // import custom hook
 import { useWindowWidth } from '../../services/windowWidth';
 
 // import components
-import LogoutButton from '../auth/LogoutButton';
 import Dropdown from './Dropdown';
+
+// import thunk
+import { removeUser } from '../../store/session';
 
 // import css
 import './navBar.css';
 
 const NavBar = () => {
+  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const width = useWindowWidth();
+
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleClick = () => {
+    setShowDropdown(false);
+  };
+
+  const handleLogout = () => {
+    dispatch(removeUser());
+    setShowDropdown(false);
+  };
 
   let navLinks;
 
   let loggedInLinks = (
     <>
       <NavLink
-        className='nav__item'
+        className={width > 800 ? 'nav__item' : 'nav__dropdown__item'}
         to='/users'
-        exact={true}
-        activeClassName='active'
+        exact
+        activeClassName='nav__item-active'
+        onClick={handleClick}
       >
         Users
       </NavLink>
-      <LogoutButton />
+      <button
+        className={width > 800 ? 'nav__item' : 'nav__dropdown__item'}
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
     </>
   );
 
   let loggedOutLinks = (
     <>
       <NavLink
-        className='nav__item'
+        className={width > 800 ? 'nav__item' : 'nav__dropdown__item'}
         to='/signup'
-        exact={true}
-        activeClassName='active'
+        exact
+        activeClassName='nav__item-active'
+        onClick={handleClick}
       >
         Sign Up
       </NavLink>
       <NavLink
-        className='nav__item'
+        className={width > 800 ? 'nav__item' : 'nav__dropdown__item'}
         to='/login'
-        exact={true}
-        activeClassName='active'
+        exact
+        activeClassName='nav__item-active'
+        onClick={handleClick}
       >
         Log In
       </NavLink>
@@ -57,13 +79,25 @@ const NavBar = () => {
     if (width > 800) {
       navLinks = loggedInLinks;
     } else {
-      navLinks = <Dropdown dropdownLinks={loggedInLinks} />;
+      navLinks = (
+        <Dropdown
+          dropdownLinks={loggedInLinks}
+          showDropdown={showDropdown}
+          setShowDropdown={setShowDropdown}
+        />
+      );
     }
   } else {
     if (width > 800) {
       navLinks = loggedOutLinks;
     } else {
-      navLinks = <Dropdown dropdownLinks={loggedOutLinks} />;
+      navLinks = (
+        <Dropdown
+          dropdownLinks={loggedOutLinks}
+          showDropdown={showDropdown}
+          setShowDropdown={setShowDropdown}
+        />
+      );
     }
   }
 

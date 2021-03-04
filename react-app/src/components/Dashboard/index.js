f2dbf6a91e3214c602fd78d0316de34614c4b208
Zachary <duvallzt@gmail.com>
2021-03-04 15:53:16 -0700

    Render user info on dashboard

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,7 +1,19 @@
+// import components
+import UserInfo from './UserInfo';
+
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div>
+        <h1 className='primary-title'>User Information</h1>
+        <UserInfo />
+      </div>
+      <div>
+        <h1 className='primary-title'>Clients</h1>
+      </div>
+      <div>
+        <h1 className='primary-title'>User Information</h1>
+      </div>
     </div>
   );
 }

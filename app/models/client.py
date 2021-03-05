6937f74e25a2a7cfb93d5326ac1992f66b0a0ef5
Zachary <duvallzt@gmail.com>
2021-03-05 14:17:52 -0700

    Add client model

---
+++ app/models/client.py
@@ -0,0 +1,23 @@
+from .db import db
+
+
+class Client(db.Model):
+    __tablename__ = "clients"
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    birthYear = db.Column(db.Integer, nullable=False)
+    code = db.Column(db.String(10), nullable=False)
+    curClient = db.Column(db.Boolean, nullable=False, default=True)
+
+    pro = db.relationship("User", back_populates="clients")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "userId": self.userId,
+            "birthYear": self.birthYear,
+            "code": self.code,
+            "curClient": self.curClient,
+            "pro": self.pro.to_dict(),
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -15,6 +15,8 @@ class User(db.Model, UserMixin):
     pxName = db.Column(db.String(100))
     phone = db.Column(db.String(20))
 
+    clients = db.relationship("Client", back_populates="pro")
+
     @property
     def password(self):
         return self.hashed_password

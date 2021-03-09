f129728c77bc4f2b945968d774a79c1dba288404
Zachary <duvallzt@gmail.com>
2021-03-09 15:50:25 -0700

    Add back button on single client view

--- react-app/src/components/Dashboard/Clients/ClientSingle/index.js
+++ react-app/src/components/Dashboard/Clients/ClientSingle/index.js
@@ -5,13 +5,19 @@ import NewUrlControls from './NewUrlControls';
 import { useClientsContext } from '../index';
 
 export default function ClientSingle() {
-  const { selectedClient, setShowForm } = useClientsContext();
+  const {
+    selectedClient,
+    setSelectedClient,
+    setShowForm,
+  } = useClientsContext();
 
   return (
     <>
       <div className='dashboard__sub-section dashboard__single-client'>
         <div className='dashboard__data'>
           <p>{selectedClient.code}</p>
+          <p>Born: {selectedClient.birthYear}</p>
+          <p>Status: {selectedClient.curClient ? 'Active' : 'Terminated'}</p>
         </div>
         <div className='dashboard__buttons'>
           <button className='secondary-button dashboard__button'>
@@ -22,6 +28,15 @@ export default function ClientSingle() {
             onClick={() => setShowForm(true)}
           >
             Update
+          </button>
+          <button
+            className='secondary-button dashboard__button'
+            onClick={() => {
+              setSelectedClient(null);
+              setShowForm(false);
+            }}
+          >
+            Back
           </button>
         </div>
         <NewUrlControls />

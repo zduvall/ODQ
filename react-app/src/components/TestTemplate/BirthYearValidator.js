0d9579af6869ef882374860bbbaf4f21632e0968
Zachary <duvallzt@gmail.com>
2021-03-11 14:04:21 -0700

    Set up form for validating birth year

--- react-app/src/components/TestTemplate/BirthYearValidator.js
+++ react-app/src/components/TestTemplate/BirthYearValidator.js
@@ -1,10 +1,34 @@
-export default function BirthYearValidator({ setShowTest }) {
+import { useState } from 'react';
+
+export default function BirthYearValidator({ setShowTest, clientId }) {
+  const [year, setYear] = useState();
+
+  async function handleClick() {
+    const validated = await fetch(`/api/clients/check-year/${clientId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: year,
+    });
+    if (validated) setShowTest(true);
+  }
+
   return (
     <div className='site__sub-section'>
-      <p>Birth Year Validator</p>
-      <button className='primary-button' onClick={() => setShowTest(true)}>
-        Validate
-      </button>
+      <p>Please confirm your birth year</p>
+      <form className='form'>
+        <input
+          className='form__input'
+          type='number'
+          placeholder='birth year'
+          value={year}
+          onChange={(e) => setYear(e.target.value)}
+        ></input>
+        <button className='primary-button' onClick={() => setShowTest(true)}>
+          Validate
+        </button>
+      </form>
     </div>
   );
 }

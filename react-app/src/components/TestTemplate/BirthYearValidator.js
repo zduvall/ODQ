feab9efeb104138070d2b299a8f791d313bdb7fd
Zachary <duvallzt@gmail.com>
2021-03-11 20:26:41 -0700

    Style confirm birth year

--- react-app/src/components/TestTemplate/BirthYearValidator.js
+++ react-app/src/components/TestTemplate/BirthYearValidator.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
 export default function BirthYearValidator({ setShowTest, clientId }) {
-  const [year, setYear] = useState('');
+  const [year, setYear] = useState();
   const [wrongYear, setWrongYear] = useState(false);
 
   async function handleClick(e) {
     e.preventDefault();
+    if (!year) {
+      setWrongYear(true);
+      return;
+    }
     const res = await fetch(`/api/clients/check-year/${clientId}/${year}`);
     const validated = await res.json();
     if (validated) setShowTest(true);
@@ -16,22 +20,28 @@ export default function BirthYearValidator({ setShowTest, clientId }) {
     <div className='site__sub-section birth-year-validator__container'>
       <h3 className='primary-title'>Please confirm your birth year</h3>
       <form className='form'>
-        <input
-          className='form__input'
-          type='number'
-          placeholder='birth year'
-          value={year}
-          onChange={(e) => setYear(e.target.value)}
-        ></input>
-        <button type='submit' className='primary-button' onClick={handleClick}>
-          Validate
-        </button>
+        <div className='birth-year-validator__form__row'>
+          <input
+            className='form__input'
+            type='number'
+            placeholder='birth year'
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+          ></input>
+          <button
+            type='submit'
+            className='primary-button'
+            onClick={handleClick}
+          >
+            Validate
+          </button>
+        </div>
       </form>
       {wrongYear && (
         <div className='errors-container'>
-          <p>
-            Invalid year entered. If you believe there is a mistake, please
-            contact your provider.
+          <p className='birth-year-validator__error'>
+            The year provided was invalid. If you believe that is a mistake,
+            please contact your provider.
           </p>
         </div>
       )}

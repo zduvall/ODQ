5536a42a4322de10e88e14235e419c33a446aa5e
Zachary <duvallzt@gmail.com>
2021-03-13 17:45:06 -0700

    Update cinfo on create client form info

--- react-app/src/components/Dashboard/Clients/ClientForm/ModalClientFormInfo/ModalClientFormInfo.js
+++ react-app/src/components/Dashboard/Clients/ClientForm/ModalClientFormInfo/ModalClientFormInfo.js
@@ -1,11 +1,13 @@
 export default function NewUrl() {
   return (
     <>
-      <h2 className='primary-title cntr-txt-sml-margin'>Adding and updating clients</h2>
+      <h2 className='primary-title cntr-txt-sml-margin'>
+        Adding and updating clients
+      </h2>
       <p>
-        DOT-21 only stores the client's birth year and a unique auto-generated
-        code based on the first name, last name, and creation date of the
-        client. This protects client confidentiality, and the code can be
+        DOT-21 does not store any HIPAA identifiers to protect confidentiality,
+        only the client's birth year and a basic auto-generated code based on
+        first name, last name, and creation date of the client. The code can be
         updated as needed by changing the respective inputs.
       </p>
     </>

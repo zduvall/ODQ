81811b65c86fe4e95a0d1a0f610575c34d1efbf3
Zachary <duvallzt@gmail.com>
2021-03-15 07:19:06 -0600

    Make first draft of terms and conditions

---
+++ react-app/src/components/auth/TermsAndConditions.js
@@ -0,0 +1,32 @@
+export default function TermsAndConditions() {
+  return (
+    <div className='site__sub-section'>
+      <h2 className='primary-title'>Terms of Use Agreement</h2>
+      <h3>General</h3>
+      <p>
+        Information on this site is for general informational and educational
+        and professional purposes. By accessing and using this Web site, you
+        accept, without limitation or qualification, this Terms of Use
+        Agreement.
+      </p>
+      <h3>Mental Health Information</h3>
+      <p>
+        This site may contain information about mental health conditions and
+        treatment. This is not meant as a substitute for advice from qualified
+        mental health or other health care professionals. Information on this
+        site should not be used by patients to self-diagnose a mental health or
+        other condition. Patients should always consult with a mental health or
+        other health care professional for advice or information about a
+        diagnosis and treatment.
+      </p>
+      <p>
+        Many questionnaires on this site are based on patient self-report. As
+        such, all responses should be reviewed and interpreted by the a
+        qualified mental health or other health care professional. Any
+        definitive diagnosis should be made on clinical grounds, factoring in
+        the patient's understanding of the questionaire and any other relevant
+        biopsychosocial information from the patient.
+      </p>
+    </div>
+  );
+}

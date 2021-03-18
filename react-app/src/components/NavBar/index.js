5dc2a7c5956d09627d0538b2f57936d9f570cdf6
Zachary <duvallzt@gmail.com>
2021-03-17 20:53:17 -0600

    Add demo login

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -115,10 +115,10 @@ const NavBar = () => {
         onClick={() => history.push('/')}
       >
         <img className='logo' src={dotLogo} alt='DOT logo' />
-        <span className='secondary-title'>D</span>iagnostic &{' '}
-        <span className='secondary-title'>O</span>utcome{' '}
-        <span className='secondary-title'>T</span>esting -{' '}
-        <span className='secondary-title'>21</span>
+        <span className='s-h__t-emph'>D</span>iagnostic &{' '}
+        <span className='s-h__t-emph'>O</span>utcome{' '}
+        <span className='s-h__t-emph'>T</span>esting -{' '}
+        <span className='s-h__t-emph'>21</span>
       </div>
       <nav className='nav'>
         <ul className='nav__wrapper'>{navLinks}</ul>

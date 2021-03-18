f871164e61cf193d0c23af2916d64984673680b2
Zachary <duvallzt@gmail.com>
2021-03-17 21:55:21 -0600

    Incorporate custom modal for delete data point (test res)

--- react-app/src/components/ClientTests/SelectedDataPoint/index.js
+++ react-app/src/components/ClientTests/SelectedDataPoint/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 //import context
@@ -5,6 +6,7 @@ import { useClientTestsContext } from '../index';
 
 // import component
 import Section from './SectionAndQuestionTypes';
+import ModalConfirmButton from '../../ModalConfirmButton';
 
 // import thunk
 import { deleteTest } from '../../../store/tests';
@@ -13,20 +15,30 @@ export default function SelectedDataPoint() {
   const dispatch = useDispatch();
   const { selectedTest, setDatapoint, datapoint } = useClientTestsContext();
 
+  const [showModal, setShowModal] = useState(false);
+
   const resObj = JSON.parse(datapoint.res);
 
   async function handleDelete() {
-    const confirm = window.confirm('Are you sure?');
-    if (confirm) {
-      await dispatch(deleteTest(datapoint.id));
-      setDatapoint(false);
-    }
+    // const confirm = window.confirm('Are you sure?');
+    // if (confirm) {
+    await dispatch(deleteTest(datapoint.id));
+    setDatapoint(false);
+    // }
   }
 
   console.log(datapoint);
 
   return (
     <div className='site__sub-section selected-data-point'>
+      <ModalConfirmButton
+        showModal={showModal}
+        setShowModal={setShowModal}
+        proceedAction={handleDelete}
+        message={`Are you sure you want to delete this test result from ${new Date(
+          datapoint.timeComp
+        ).toLocaleDateString()}? You should only do this if there is an error in the client response.`}
+      />
       <h3 className='cntr-txt-sml-margin primary-title'>
         {new Date(datapoint.timeComp).toLocaleDateString()}
       </h3>
@@ -35,7 +47,7 @@ export default function SelectedDataPoint() {
           return <Section key={section.id} section={section} resObj={resObj} />;
         })}
       </div>
-      <button onClick={handleDelete} className='delete-button'>
+      <button onClick={() => setShowModal(true)} className='delete-button'>
         Delete
       </button>
     </div>

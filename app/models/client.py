29059500617d6ef89466df4b019075ae31ca6b82
Zachary <duvallzt@gmail.com>
2021-03-23 12:17:16 -0600

    Add tests onto to_dict() of client

--- app/models/client.py
+++ app/models/client.py
@@ -23,4 +23,5 @@ class Client(db.Model):
             "code": self.code,
             "curClient": self.curClient,
             "pro": self.pro.to_dict(),
+            "tests": [test.to_dict() for test in self.tests],
         }

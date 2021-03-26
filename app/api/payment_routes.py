a4cad35548eb152d54c3d710ba7252b721d14d9f
Zachary <duvallzt@gmail.com>
2021-03-26 01:11:52 -0600

    Add in comments to remind me where I left off

--- app/api/payment_routes.py
+++ app/api/payment_routes.py
@@ -18,56 +18,59 @@ stripe.api_key = stripe_keys["secret_key"]
 payment_routes = Blueprint("payments", __name__)
 
 
-@payment_routes.route("/config")
-def get_publishable_key():
-    stripe_config = {"publicKey": stripe_keys["publishable_key"]}
-    return jsonify(stripe_config)
+# from: https://testdriven.io/blog/flask-stripe-subscriptions/ -- got up to 'AJAX Request'
+
+# @payment_routes.route("/config")
+# def get_publishable_key():
+#     stripe_config = {"publicKey": stripe_keys["publishable_key"]}
+#     return jsonify(stripe_config)
 
 
-# from: https://stripe.com/docs/billing/subscriptions/fixed-price
+# from: https://stripe.com/docs/billing/subscriptions/fixed-price -- in the middle of '4 Create Stripe Customer'
 
-# @payment_routes.route("/stripe-webhook", methods=["POST"])
-# def webhook_received():
-#     # You can use webhooks to receive information about asynchronous payment events.
-#     # For more about our webhook events check out https://stripe.com/docs/webhooks.
-#     webhook_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
-#     request_data = json.loads(request.data)
 
-#     if webhook_secret:
-#         # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
-#         signature = request.headers.get("stripe-signature")
-#         try:
-#             event = stripe.Webhook.construct_event(
-#                 payload=request.data, sig_header=signature, secret=webhook_secret
-#             )
-#             data = event["data"]
-#         except Exception as e:
-#             return e
-#         # Get the type of webhook event sent - used to check the status of PaymentIntents.
-#         event_type = event["type"]
-#     else:
-#         data = request_data["data"]
-#         event_type = request_data["type"]
+@payment_routes.route("/stripe-webhook", methods=["POST"])
+def webhook_received():
+    # You can use webhooks to receive information about asynchronous payment events.
+    # For more about our webhook events check out https://stripe.com/docs/webhooks.
+    webhook_secret = os.getenv("STRIPE_WEBHOOK_SECRET")
+    request_data = json.loads(request.data)
 
-#     data_object = data["object"]
+    if webhook_secret:
+        # Retrieve the event by verifying the signature using the raw body and secret if webhook signing is configured.
+        signature = request.headers.get("stripe-signature")
+        try:
+            event = stripe.Webhook.construct_event(
+                payload=request.data, sig_header=signature, secret=webhook_secret
+            )
+            data = event["data"]
+        except Exception as e:
+            return e
+        # Get the type of webhook event sent - used to check the status of PaymentIntents.
+        event_type = event["type"]
+    else:
+        data = request_data["data"]
+        event_type = request_data["type"]
 
-#     if event_type == "invoice.paid":
-#         # Used to provision services after the trial has ended.
-#         # The status of the invoice will show up as paid. Store the status in your
-#         # database to reference when a user accesses your service to avoid hitting rate
-#         # limits.
-#         print(data)
+    data_object = data["object"]
 
-#     if event_type == "invoice.payment_failed":
-#         # If the payment fails or the customer does not have a valid payment method,
-#         # an invoice.payment_failed event is sent, the subscription becomes past_due.
-#         # Use this webhook to notify your user that their payment has
-#         # failed and to retrieve new card details.
-#         print(data)
+    if event_type == "invoice.paid":
+        # Used to provision services after the trial has ended.
+        # The status of the invoice will show up as paid. Store the status in your
+        # database to reference when a user accesses your service to avoid hitting rate
+        # limits.
+        print(data)
 
-#     if event_type == "customer.subscription.deleted":
-#         # handle subscription cancelled automatically based
-#         # upon your subscription settings. Or if the user cancels it.
-#         print(data)
+    if event_type == "invoice.payment_failed":
+        # If the payment fails or the customer does not have a valid payment method,
+        # an invoice.payment_failed event is sent, the subscription becomes past_due.
+        # Use this webhook to notify your user that their payment has
+        # failed and to retrieve new card details.
+        print(data)
 
-#     return jsonify({"status": "success"})
+    if event_type == "customer.subscription.deleted":
+        # handle subscription cancelled automatically based
+        # upon your subscription settings. Or if the user cancels it.
+        print(data)
+
+    return jsonify({"status": "success"})
--- react-app/src/services/stripeUtils.js
+++ react-app/src/services/stripeUtils.js
@@ -11,3 +11,25 @@ export default async function createCustomer(billingEmail) {
   const { customer } = res.json();
   return customer;
 }
+
+// // if above doesn't work, switch to this -- using .then()
+
+// function createCustomer() {
+//   let billingEmail = document.querySelector('#email').value;
+//   return fetch('/create-customer', {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       email: billingEmail,
+//     }),
+//   })
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((result) => {
+//       // result.customer.id is used to map back to the customer object
+//       return result;
+//     });
+// }

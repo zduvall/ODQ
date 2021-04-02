1fe259a9564c9024342554c5c393c700d9375644
Zachary <duvallzt@gmail.com>
2021-04-01 18:28:59 -0700

    Remove unneeded code

--- react-app/src/pages/Payments.js
+++ react-app/src/pages/Payments.js
@@ -1,18 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-// import thunk
-import { togglePremium } from '../store/session';
-
 // import components
 import Payment from '../components/Profile/Payment';
 
 export default function Payments() {
-  const dispatch = useDispatch();
-  const sessionUser = useSelector((state) => state.session.user);
-
-  const handleToggleSubscribe = async (newPremium = !sessionUser.premium) => {
-    await dispatch(togglePremium(sessionUser.id, newPremium));
-  };
 
   return (
     <div>

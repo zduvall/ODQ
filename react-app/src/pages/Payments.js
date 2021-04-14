a3af819229fc7af4db391f00c1bca0dcabfb0d42
Zachary <duvallzt@gmail.com>
2021-04-14 14:31:29 -0600

    Redirect to account page if try to access payments while already subscribed

--- react-app/src/pages/Payments.js
+++ react-app/src/pages/Payments.js
@@ -1,5 +1,6 @@
-import { useState, useContext, createContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useContext, createContext, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
 
 // import components
 import Payment1 from '../components/Payments/Payment1';
@@ -7,16 +8,24 @@ import Payment2 from '../components/Payments/Payment2';
 import Payment3 from '../components/Payments/Payment3';
 
 // import css
-import '../components/Payments/Payments.css'
+import '../components/Payments/Payments.css';
 
 const PaymentsContext = createContext();
 export const usePaymentsContext = () => useContext(PaymentsContext);
 
 export default function Payments() {
+  const history = useHistory();
+  const sessionUser = useSelector((state) => state.session.user);
+
   const { subPageId } = useParams('subPageId');
 
   const [billingInfo, setBillingInfo] = useState('');
   const [paymentMethod, setPaymentMethod] = useState('');
+
+  // redirect to account page if try to access payments page while already subscribed
+  useEffect(() => {
+    if (sessionUser.subType) history.push('/account');
+  }, [sessionUser, history]);
 
   return (
     <PaymentsContext.Provider
@@ -25,9 +34,9 @@ export default function Payments() {
       <div className='site__page'>
         <h1 className='primary-title'>Premium Subscription</h1>
         {/* <div className='site__sub-section form-container'> */}
-          {subPageId === '1' && <Payment1 />}
-          {subPageId === '2' && <Payment2 />}
-          {subPageId === '3' && <Payment3 />}
+        {subPageId === '1' && <Payment1 />}
+        {subPageId === '2' && <Payment2 />}
+        {subPageId === '3' && <Payment3 />}
         {/* </div> */}
         <div className='one1rem-ht' />
       </div>

710892c6541e4d2f5c80834e9b080bc906759a72
Zachary <duvallzt@gmail.com>
2021-04-14 14:21:27 -0600

    Make own component to show demo video

---
+++ react-app/src/components/auth/DemoVideo.js
@@ -0,0 +1,16 @@
+export default function DemoVideo() {
+  return (
+    <p className='auth__sml-txt'>
+      Checkout the{' '}
+      <a
+        className='clickable-link'
+        href='https://youtu.be/Ycli5-38V-Q'
+        target='_blank'
+        rel='noreferrer'
+      >
+        demo video
+      </a>
+      .
+    </p>
+  );
+}

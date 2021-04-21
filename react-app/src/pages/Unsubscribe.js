da80d81223fa932768f3a65a234bdd796c6f268c
Zachary <duvallzt@gmail.com>
2021-04-21 12:15:02 -0600

    Add image to unsubscribe page

--- react-app/src/pages/Unsubscribe.js
+++ react-app/src/pages/Unsubscribe.js
@@ -1,6 +1,14 @@
+import goodBye from '../components/Unsubscribe/images/leaving-through-door.svg';
+
 export default function Unsubscribe() {
   return (
     <div className='site__page'>
+      <img
+        className='splash__secondary-image'
+        src={goodBye}
+        alt={'Man exiting through door'}
+        title={'Man exiting through door'}
+      />
       <div className='site__sub-section'></div>
     </div>
   );

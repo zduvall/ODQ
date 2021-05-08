a53a1e6e40676c48cd4947fd5a536a7b11e5aa70
Zachary <duvallzt@gmail.com>
2021-05-08 17:10:45 -0600

    Add SVG for general feedback image

--- react-app/src/pages/Feedback.js
+++ react-app/src/pages/Feedback.js
@@ -6,8 +6,9 @@ import FeedbackForm from '../components/Feedback/FeedbackForm';
 
 // images
 import goodByeImg from '../components/Feedback/images/leaving-through-door.svg';
-import LeaveThroughDoorSVG from '../components/Feedback/images/LeaveThroughDoorSVG';
 import feedbackImg from '../components/Feedback/images/undraw_Redesign_feedback.svg';
+import LeaveThroughDoorSVG from '../components/Feedback/images/LeaveThroughDoorSVG';
+import GeneralFeedbackSVG from '../components/Feedback/images/GeneralFeedbackSVG';
 
 // css
 import '../components/Feedback/Feedback.css';
@@ -49,13 +50,8 @@ export default function Unsubscribe() {
       <FeedbackForm type={pathname.slice(1)} />
       <div className='one1rem-ht' />
       <div className='site__sub-section'>
-        <img
-          className='feedback-image'
-          src={pathname === '/feedback' ? feedbackImg : goodByeImg}
-          alt={'Person exiting through door'}
-          title={'Person exiting through door'}
-        />
         {pathname !== '/feedback' && <LeaveThroughDoorSVG />}
+        {pathname === '/feedback' && <GeneralFeedbackSVG />}
       </div>
     </div>
   );

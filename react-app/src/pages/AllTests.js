1a0832df2669d343080bc3acb8c86917287bfd1c
Zachary <duvallzt@gmail.com>
2021-05-12 19:24:30 -0600

    Render test dropdowns

--- react-app/src/pages/AllTests.js
+++ react-app/src/pages/AllTests.js
@@ -1,10 +1,18 @@
-
+import tests from '../assets/index';
 
 export default function AllTests() {
   return (
     <div>
       <h1>All Tests</h1>
-      <h2>tests</h2>
+      {Object.values(tests).map((test) => (
+        <>
+          <details>
+            <summary>{test.name}</summary>
+            <p>{test.description}</p>
+            <p>{test.interpretation}</p>
+          </details>
+        </>
+      ))}
     </div>
   );
 }

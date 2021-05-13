11006b5690ba504b3c556f989d486473cb93c114
Zachary <duvallzt@gmail.com>
2021-05-13 15:31:00 -0600

    Create and import css file

--- react-app/src/pages/AllTests.js
+++ react-app/src/pages/AllTests.js
@@ -4,13 +4,18 @@ import tests from '../assets/index';
 // import component
 import SingleTestDetails from '../components/AllTests';
 
+// import css
+import '../components/AllTests/AllTests.css'
+
 export default function AllTests() {
   return (
     <div className='site__page'>
       <h1 className='primary-title'>Available Tests</h1>
-      {Object.values(tests).map((test) => (
-        <SingleTestDetails test={test} />
-      ))}
+      <div className='site__sub-section'>
+        {Object.values(tests).map((test) => (
+          <SingleTestDetails test={test} />
+        ))}
+      </div>
     </div>
   );
 }

923bec6544c30e47d8d6d41e4a7ea9136a5198bd
Zachary <duvallzt@gmail.com>
2021-05-13 15:57:18 -0600

    Dry up code by reusing component

--- react-app/src/components/Client/DropdownTestInfo.js
+++ react-app/src/components/Client/DropdownTestInfo.js
@@ -1,20 +1,13 @@
-import { useSelector } from 'react-redux';
+//import component
+import TestInfo from '../AllTests/TestInfo';
 
 // import context
 import { useClientTestsContext } from '../../pages/Client';
-
-// import check subscription type function
-import checkSubType from '../../services/checkSubType';
-
-// import free tests
-import { freeTests } from '../../assets';
 
 // import css
 import './Client.css';
 
 export default function DropdownTestInfo() {
-  const subType = useSelector((state) => state.session.user.subType);
-
   const { dropdownTest, setDropdownTest } = useClientTestsContext();
 
   return (
@@ -23,52 +16,7 @@ export default function DropdownTestInfo() {
         className='fas fa-times top-right-grey'
         onClick={() => setDropdownTest({ code: '' })}
       ></i>
-      <h2 className='primary-title cntr-txt-sml-margin'>
-        {dropdownTest.name}{' '}
-        {!freeTests.includes(dropdownTest.code) && (
-          <i
-            title={
-              checkSubType(dropdownTest.code, subType)
-                ? 'As a s subscribing user, you have access to premium tests.'
-                : 'Subscribing users have access to premium tests'
-            }
-            className='fas fa-medal medal-w-title'
-          ></i>
-        )}
-      </h2>
-      <h3 className='tertiary-title cntr-txt-sml-margin'>
-        ({dropdownTest.abbr})
-      </h3>
-      <div className='test-descriptions-text'>
-        <p className='new-line-on-slash-n'>
-          <span className='underline bold'>Description</span>:{' '}
-          {dropdownTest.description}
-        </p>
-        <p className='new-line-on-slash-n'>
-          <span className='underline bold'>Scoring</span>: {dropdownTest.score}
-        </p>
-        <p className='new-line-on-slash-n'>
-          <span className='underline bold'>Interpretation</span>:{' '}
-          {dropdownTest.interpretation}
-        </p>
-        <p>
-          <span className='underline bold'>Completion</span>:{' '}
-          {dropdownTest.minMinutes} - {dropdownTest.maxMinutes} minutes,{' '}
-          {dropdownTest.selfAdmin
-            ? 'self-administered'
-            : 'taken by professional'}
-        </p>
-        <p className='new-line-on-slash-n'>
-          <span className='underline bold'>Attribution</span>:{' '}
-          {dropdownTest.attribution}
-        </p>
-        <p
-          className='clickable-link'
-          onClick={() => window.open(dropdownTest.link)}
-        >
-          More Information
-        </p>
-      </div>
+      <TestInfo test={dropdownTest} />
     </div>
   );
 }

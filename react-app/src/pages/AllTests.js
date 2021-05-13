70e8e14d4811f3504cfb860ce9c54640537ad563
Zachary <duvallzt@gmail.com>
2021-05-13 15:18:07 -0600

    Separate test details into component

--- react-app/src/pages/AllTests.js
+++ react-app/src/pages/AllTests.js
@@ -1,17 +1,15 @@
+// import test objects and array of premium tests
 import tests from '../assets/index';
+
+// import component
+import SingleTestDetails from '../components/AllTests';
 
 export default function AllTests() {
   return (
-    <div>
-      <h1>All Tests</h1>
+    <div className='site__page'>
+      <h1 className='primary-title'>Available Tests</h1>
       {Object.values(tests).map((test) => (
-        <>
-          <details>
-            <summary>{test.name}</summary>
-            <p>{test.description}</p>
-            <p>{test.interpretation}</p>
-          </details>
-        </>
+        <SingleTestDetails test={test} />
       ))}
     </div>
   );

79dab3b00c921537b95047a552a16e6ede878352
Zachary <duvallzt@gmail.com>
2021-06-03 17:55:09 -0600

    Update intro info

--- react-app/src/components/TestForm/TestIntro.js
+++ react-app/src/components/TestForm/TestIntro.js
@@ -3,8 +3,8 @@ export default function TestIntro() {
     <div className='site__sub-section'>
       <p>
         Your therapist sent this link from eDOT to help measure and track your
-        progress. Using these evidence based measurement tools can help you and
-        your therapist be best equipped to facilitate your progress.
+        progress. Using these evidence based measurement tools can help equip
+        you and your therapist to have better and quicker positive outcomes.
       </p>
     </div>
   );

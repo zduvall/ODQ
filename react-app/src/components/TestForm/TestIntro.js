b7d9b3a28e650b9457b24b537bf525bed38a1003
Zachary <duvallzt@gmail.com>
2021-06-03 18:59:08 -0600

    Update intro text

--- react-app/src/components/TestForm/TestIntro.js
+++ react-app/src/components/TestForm/TestIntro.js
@@ -1,12 +1,14 @@
 export default function TestIntro({ length }) {
   return (
     <div className='site__sub-section flex-dir-col'>
-      <p>
+      <p className='cntr-txt-sml-margin'>
+        Thank you for taking {length} to complete this questionnaire!
+      </p>
+      <p className='cntr-txt-sml-margin'>
         Your therapist sent this link from eDOT to help measure and track your
         progress. Using these evidence based measurement tools can help equip
         you and your therapist to have better and quicker positive outcomes.
       </p>
-      <p>Thank you for taking {length} to complete this questionnaire!</p>
     </div>
   );
 }

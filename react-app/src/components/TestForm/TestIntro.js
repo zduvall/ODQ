0ca5b14baefeb0ca162c64004506ede4585f6e14
Zachary <duvallzt@gmail.com>
2021-06-04 23:34:11 -0600

    Update intro message

--- react-app/src/components/TestForm/TestIntro.js
+++ react-app/src/components/TestForm/TestIntro.js
@@ -5,9 +5,9 @@ export default function TestIntro({ length }) {
         Thank you for taking {length} to complete this questionnaire!
       </p>
       <p className='cntr-txt-sml-margin'>
-        Your therapist sent this questionnaire from eDOT to help measure and
+        Your mental health provider sent this from eDOT to help measure and
         track your progress. Using evidence-based tools like this can help you
-        reach better and quicker positive outcomes and equip your therapist to
+        reach better and quicker positive outcomes and equip your provider to
         best help you.
       </p>
     </div>

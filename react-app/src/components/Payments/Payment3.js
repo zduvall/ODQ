0cc3f941a732d394fa3d5909edf53d5aa569e7b1
Zachary <duvallzt@gmail.com>
2021-06-07 13:28:05 -0700

    Add comment to third payment component

--- react-app/src/components/Payments/Payment3.js
+++ react-app/src/components/Payments/Payment3.js
@@ -1,3 +1,5 @@
+// this is the component to confirm subscription
+
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';

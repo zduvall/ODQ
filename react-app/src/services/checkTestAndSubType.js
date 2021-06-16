ac6801df5ad6ca6f71c5b52eb875038c71503489
Zachary <duvallzt@gmail.com>
2021-06-15 23:31:52 -0600

    Update comment

--- react-app/src/services/checkTestAndSubType.js
+++ react-app/src/services/checkTestAndSubType.js
@@ -9,4 +9,3 @@ export default function checkTestAndSubType(code, subType) {
 }
 
 // I need to also make this check if the subscription is active
-// also maybe I should rename this function? because it's also checking if a test (don't remember why)
